ff524504973a69c50c301e7f707964d468d2a613
Pedro Henrique <lairpedroph@gmail.com>
2019-09-25 23:41:28 -0300

    Criei um selecionador de numeros primos que coleta sua menor raiz primitiva, atualizei os metodos para ficar mais coeso, modifiquei o nome de ada para alice, remodelagem do funcionamento

--- alice.py
+++ alice.py
@@ -1,8 +1,8 @@
 import random
 
-class Bob:
+class Alice:
     def __init__(self, tf, ccp, g, p):
-        self.SIMBOLO = "-"
+        self.SIMBOLO = "+"
         self.g = g
         self.p = p
 
@@ -12,40 +12,40 @@ class Bob:
         self.tf = tf
         self.caminho_chave_privada = ccp
 
-        self.numero_secreto_b = random.randrange(1, p - 1)
+        self.numero_secreto_a = random.randrange(1, p - 1)
 
         f = open(ccp, "a+")
         f.write("Meu gerador = " + str(self.g) + "\n")
         f.write("Meu numero primo = " + str(self.p) + "\n")
-        f.write("Meu numero secreto b = " + str(self.numero_secreto_b) + "\n")
+        f.write("Meu numero secreto a = " + str(self.numero_secreto_a) + "\n")
         f.close()
 
-    def calcula_e_grava_B(self):
-        self.B = (self.g**self.numero_secreto_b)%self.p
+    def calcula_e_grava_A(self):
+        self.A = (self.g**self.numero_secreto_a)%self.p
 
         f = open(self.tf, "a+")
-        f.write("B= " + str(self.B) + "\n")
+        f.write("A= " + str(self.A) + "\n")
         f.close()
 
         f = open(self.caminho_chave_privada, "a+")
-        f.write("B= " + str(self.B) + "\n")
+        f.write("A= " + str(self.A) + "\n")
         f.close()
 
-    def ler_A(self):
+    def ler_B(self):
         with open(self.tf) as f:
             linhas = f.readlines()
 
             lista_invertida = [linhas[i] for i in range(len(linhas) - 1, -1, -1)]
             lista_invertida = [e.replace('\n', '') for e in lista_invertida]
-            lista_invertida = [e.replace('A= ', self.SIMBOLO) for e in lista_invertida]
+            lista_invertida = [e.replace('B= ', self.SIMBOLO) for e in lista_invertida]
 
             for i in range(len(lista_invertida)):
                 if lista_invertida[i][0] == self.SIMBOLO:
-                    self.A = int(lista_invertida[i][1:])
+                    self.B = int(lista_invertida[i][1:])
                     break
 
     def gravar_num_e_chave_secretos(self):
         f = open(self.caminho_chave_privada, "a+")
-        chave_secreta = (self.A**self.numero_secreto_b)%self.p
+        chave_secreta = (self.B**self.numero_secreto_a)%self.p
         f.write("A chave criptografada = " + str(chave_secreta) + "\n")
         f.close()
--- dh.py
+++ dh.py
@@ -1,40 +1,65 @@
-from ada import Ada
+from random import randrange
+from alice import Alice
 from bob import Bob
 
+def seleciona_primo():
+    with open("primos.txt") as f:
+        primos = f.readline().split()
+        return int(primos[randrange(0,len(primos))])
 
+def calcula_raiz_primitiva_e_primo():
+    while True:
+        p = seleciona_primo()
+        for g in range(2,p):
+            coprimos = set(range(1,p))
+            for i in range(1, p):
+                try:
+                    coprimos.remove((g**i) % p)
+                except:
+                    break
+            if len(coprimos) == 0:
+                f = open("canal.txt", "w")
+                f.write("O gerador = " + str(g))
+                f.write("\nO primo = " + str(p) + "\n")
+                f.close()
+                return g,p
+     
+def limpa_canais(canal, cppa, cppb):
+    open(canal, 'w').close()
+    open(cppa, 'w').close()
+    open(cppb, 'w').close()
 
-# O "canal" representa o ambiente inseguro onde haverá a comunicação entre Bob e Ada
+# O "canal" representa o ambiente inseguro onde haverá a comunicação entre Bob e Alice
 canal = "canal.txt"
 
 # O caminho_chave_privada serve para provar que seus números secretos podem ser diferentes e que resultam na mesma chave privada
 # NÃO É UTILIZADO EM UM SISTEMA DH REAL
-caminho_chave_privada_ada = "chave_privada_ada.txt"
+caminho_chave_privada_alice = "chave_privada_alice.txt"
 caminho_chave_privada_bob = "chave_privada_bob.txt"
 
-# Limpa os arquivos onde serão armazenadas as chaves privadas e os resultados trocados
-open(caminho_chave_privada_ada, "w").close()
-open(caminho_chave_privada_bob, "w").close()
+# Limpa os arquivos privados e o canal público
+limpa_canais(canal, caminho_chave_privada_alice, caminho_chave_privada_bob)
 
-ada = Ada(canal, caminho_chave_privada_ada)
-bob = Bob(canal, caminho_chave_privada_bob)
+# É selecionado aleatoriamente um número primo positivo até 1000 e calculado sua menor raiz primitiva
+(g, p) = calcula_raiz_primitiva_e_primo()
 
-# Ada e Bob vão armazenar localmente as variáveis g e p que eles escolheram pelo público
-ada.ler_g_p()
-bob.ler_g_p()
-
-# Ada e Bob vão enviar publicamente seus respectivos resultados de g^num_secreto(mod p)
-ada.gravar_A()
-bob.gravar_B()
+# São criados Alice e Bob recebendo o gerador e o número primo pelo canal público
+alice = Alice(canal, caminho_chave_privada_alice, g, p)
+bob = Bob(canal, caminho_chave_privada_bob, g, p)
 input()
 
-# Ada e Bob vão ler os resultados um do outro que foram passados pelo rede pública
-ada.ler_B()
+# Ada e Bob vão armazenar localmente as variáveis g e p que eles escolheram pelo público
+alice.calcula_e_grava_A()
+bob.calcula_e_grava_B()
+input()
+
+# Ada e Bob vão ler os resultados das chaves intermediárias um do outro que foram passados pela rede pública
+alice.ler_B()
 bob.ler_A()
 
-# Os números secretos são salvos para provar que podem ser diferentes e mesmo assim a chave privada é identica
-ada.gravar_num_secreto()
-bob.gravar_num_secreto()
-
+# Os números secretos são salvos para provar que mesmo podendo ser diferentes, a chave privada é idêntica
 # Ada e Bob vão gravar localmente sua chave privada, que será usada para descriptografar as mensagens
-ada.calcula_chave_secreta()
-bob.calcula_chave_secreta()
+alice.gravar_num_e_chave_secretos()
+bob.gravar_num_e_chave_secretos()
+
+
